07c0662247dfbbc7d1697d16510073742928fbbe
Isaias Chavarin <isaiascv781@gmail.com>
2024-02-18 20:08:43 -0800

    Some done

--- CSC311-S24-STUDENT/Assignment1/assignment1/NumberArrayList.java
+++ CSC311-S24-STUDENT/Assignment1/assignment1/NumberArrayList.java
@@ -3,28 +3,36 @@ package assignment1;
 
 public class NumberArrayList 
 {
+	private int maxSize
 	private int size;
 	private int[] myArray;
 	
 	public NumberArrayList(int size)
 	{
-		this.size =size;
+		this.myArray = new int[size];
 		
 	}
 	
 	public int add(int number)
 	{
-		
+		if (size < maxSize)
+		{
+			myArray[size++] = number;
+		}
+		return size-1;
 	}
 	
 	public int get(int index)
 	{
-		
+		if (index >= 0 && index < size)
+		{
+			return myArray[index];
+		}
 	}
 	
 	public int size()
 	{
-		
+		return size;
 	}
 	
 	public int[] find(int number)
