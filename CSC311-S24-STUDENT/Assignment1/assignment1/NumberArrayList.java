42bdef42de86598e48f384076c9661ba5bba0927
Isaias Chavarin <isaiascv781@gmail.com>
2024-02-18 19:52:54 -0800

    Layout of code

--- CSC311-S24-STUDENT/Assignment1/assignment1/NumberArrayList.java
+++ CSC311-S24-STUDENT/Assignment1/assignment1/NumberArrayList.java
@@ -1,5 +1,49 @@
 package assignment1;
+//Isaias Chavarin Velasquez (ichavarinvelasquez1@toromail.csudh.edu)
 
-public class NumberArrayList {
-
+public class NumberArrayList 
+{
+	private int size;
+	private int[] myArray;
+	
+	public NumberArrayList(int size)
+	{
+		this.size =size;
+		
+	}
+	
+	public int add(int number)
+	{
+		
+	}
+	
+	public int get(int index)
+	{
+		
+	}
+	
+	public int size()
+	{
+		
+	}
+	
+	public int[] find(int number)
+	{
+		
+	}
+	
+	public int getLargest()
+	{
+		
+	}
+	
+	public int getSmallest()
+	{
+		
+	}
+	
+	public int getAverage()
+	{
+		
+	}
 }
